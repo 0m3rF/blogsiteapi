1349afeabea42be5301863236244890be118cfb6
Ömer Faruk Kırlı <omerfarukkirli1994@gmail.com>
2017-03-06 02:11:48 +0300

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var app = express();
+
+var mongodbString = "";
+var port = process.env.PORT || 3000 ;
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => { console.log("App startred to listen port = " + port );});
+
+
+
+
+
+
